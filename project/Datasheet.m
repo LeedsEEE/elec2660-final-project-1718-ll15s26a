49feb5c8d46aa18ec3dc17704e257c59387956be
Samia Abuzant <samzant17@Samias-Mac.local>
2017-12-06 00:15:58 +0000

    calculator functioning correctly along with ELEC2530.........ELEC2240 still left to be resolved.......data type issue

---
+++ project/Datasheet.m
@@ -0,0 +1,29 @@
+//
+//  Datasheet.m
+//  project
+//
+//  Created by Samia Abuzant on 05/12/2017.
+//  Copyright © 2017 Samia Abuzant. All rights reserved.
+//
+
+#import "Datasheet.h"
+
+@interface Datasheet ()
+
+@end
+
+@implementation Datasheet
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+   
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+@end
+
